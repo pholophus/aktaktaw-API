b33b3fadedc451d85a9b1a4201194c587c2b33d0
pholophus <iqbalkisas6@gmail.com>
2019-10-20 12:56:00 +0800

    latest update

--- database/migrations/2019_10_20_011119_create_investors_table.php
+++ database/migrations/2019_10_20_011119_create_investors_table.php
@@ -19,6 +19,7 @@ class CreateInvestorsTable extends Migration
             $table->integer('quantity');
             $table->text('fromWhere');
             $table->integer('investment')->default(200000);
+            $table->double('commission', 8, 2)->default(0.5);
             $table->timestamps();
             $table->softDeletes();
         });
